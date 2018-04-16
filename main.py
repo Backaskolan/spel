9bf896ca07974ab64dd9ac9f1afe3a92c9f93131
Linus Rundberg Streuli <linus.rundberg@backaskolan.se>
2018-04-16 10:45:24 +0200

    lade till user input

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+#!/usr/bin/python3
+
 import random
 import time
 
+def quit_game():
+    print('Tack för att du spelade.')
+    exit()
+
 class GameObject:
-    def __init__(self, name, fighter=None):
+    def __init__(self, name, fighter=None, input_component=None):
         self.name = name
         self.fighter = fighter
+        self.input_component = input_component
         if self.fighter:
             self.fighter.owner = self
+        if self.input_component:
+            self.input_component.owner = self
 
 class Fighter:
     def __init__(self, hp, max_damage):
@@ -19,19 +28,56 @@ class Fighter:
         other.fighter.hp = other.fighter.hp - damage
         print('{player} gör {damage} skada på {other}. {other} har {hp} hp kvar.'.format(player=self.owner.name, damage=damage, other=other.name, hp=other.fighter.hp))
 
-fighter_component = Fighter(hp=34, max_damage=12)
-player = GameObject(name='Ewert', fighter=fighter_component)
+    def flee(self):
+        print(f'{self.owner.name} flyr med svansen mellan benen.')
+        quit_game()
+
+class Action:
+    def __init__(self, name, action, target=None):
+        self.name = name
+        self.action = action
+        self.target = target
+
+class InputHandler:
+    def __init__(self, owner):
+        self.owner = owner
+        self.actions = {
+            'a': Action(name='attackera', action=self.owner.fighter.attack, target=random.choice(enemies)),
+            'f': Action(name='fly', action=self.owner.fighter.flee),
+            'q': Action(name='avsluta', action=quit_game)
+        }
+
+    def player_input(self):
+        available_actions = ''
+        for k, v in self.actions.items():
+            available_actions += '{}: {} | '.format(k, v.name)
+        action = input(available_actions)
+        if action in self.actions.keys():
+            chosen_action = self.actions[action]
+            if chosen_action.target:
+                chosen_action.action(chosen_action.target)
+            else: 
+                chosen_action.action()
+        else:
+            print('Okänt kommando.')
+            return False
 
 fighter_component = Fighter(hp=8, max_damage=7)
 kobold = GameObject(name='Kobold', fighter=fighter_component)
 fighter_component = Fighter(hp=24, max_damage=9)
 ork = GameObject(name='Ork', fighter=fighter_component)
-
 enemies = [kobold, ork]
+
+fighter_component = Fighter(hp=34, max_damage=12)
+player = GameObject(name='Ewert', fighter=fighter_component)
+input_component = InputHandler(owner=player)
+player.input_component = input_component
+
+
 
 while True:
     if player.fighter.hp > 0:
-        player.fighter.attack(random.choice(enemies))
+        player.input_component.player_input()
     else:
         print('{} är död.'.format(player.name))
         break
