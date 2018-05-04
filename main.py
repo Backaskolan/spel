142ff09a93d51f3b9088ce13e151d2c198db188b
Linus Rundberg Streuli <linus.rundberg@backaskolan.se>
2018-05-04 10:21:07 +0200

    added party

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class Player:
         self.name = name
 
     def attack(self, other):
+        time.sleep(0.8)
         damage = random.randint(1, self.max_damage)
         other.hp = other.hp - damage
         print('{player} gör {damage} skada på {other}. {other} har {hp} hp kvar.'.format(player=self.name, damage=damage, other=other.name, hp=other.hp))
@@ -25,21 +26,24 @@ class Enemy:
         print('{player} gör {damage} skada på {other}. {other} har {hp} hp kvar.'.format(player=self.name, damage=damage, other=other.name, hp=other.hp))
 
 player = Player(hp=34, max_damage=12, name='Ewert')
+player2 = Player(hp=29, max_damage=15, name='Smulan')
 
 kobold = Enemy(hp=8, max_damage=7, name='Kobold')
 ork = Enemy(hp=24, max_damage=9, name='Ork')
 
+party = [player, player2]
 enemies = [kobold, ork]
 
 while True:
-    if player.hp > 0:
-        player.attack(random.choice(enemies))
-    else:
-        print('{} är död.'.format(player.name))
-        break
+    for player in party:
+        if player.hp > 0:
+            player.attack(random.choice(enemies))
+        else:
+            print('{} är död.'.format(player.name))
+            break
     for enemy in enemies:
         if enemy.hp > 0:
-            enemy.attack(player)
+            enemy.attack(random.choice(party))
         else:
             print('{} är död.'.format(enemy.name))
             enemies.remove(enemy)
